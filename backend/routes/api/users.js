840560a390682f15060b44d467bfb2afa57ee53d
Andre <andreabad17@gmail.com>
2023-04-13 15:02:56 -0700

    Add User input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,9 +4,34 @@ const bcrypt = require('bcryptjs');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+/*----------------------- Validates Sign Up -----------------------*/
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors
+];
+
+/*----------------------- Signs Up New Users -----------------------*/
+router.post('/', validateSignup, async (req, res) => {
   // Gets the infor from req.body
   const { email, password, username } = req.body;
   // Hashes the password
@@ -24,7 +49,7 @@ router.post('/', async (req, res) => {
   // creates a cookie for the user
   await setTokenCookie(res, safeUser);
 
-  // sends the info 
+  // sends the info
   return res.json({
     user: safeUser
   });
