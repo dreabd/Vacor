e28ad921e5290cd719ddbb17ff74046a2c1d3db1
Andre <andreabad17@gmail.com>
2023-04-13 14:29:19 -0700

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,7 @@
-// backend/routes/api/index.js
 const router = require('express').Router();
-const { restoreUser } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
 
 router.use(restoreUser); /*KEEP THIS FILE IN THE TOP */
 // Allows all router to retrieve the current user on teh req obj as **req.user**
@@ -8,9 +9,13 @@ router.use(restoreUser); /*KEEP THIS FILE IN THE TOP */
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
 
+router.use('/session', sessionRouter);
 
+router.use('/users', usersRouter);
 
-
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 
 
---
+++ backend/routes/api/session.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+const router = express.Router();
+/* -------------------- Log In--------------------*/
+
+router.post('/', async (req, res, next) => {
+  // Gets the crediential and password from the user
+  const { credential, password } = req.body;
+
+  // Finds a user that matches either the email or the username
+  const user = await User.unscoped().findOne({
+    where: {
+      [Op.or]: {
+        username: credential,
+        email: credential
+      }
+    }
+  });
+  // if there is a no user found or if password is not the same throws an error
+  if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+    const err = new Error('Login failed');
+    err.status = 401;
+    err.title = 'Login failed';
+    err.errors = { credential: 'The provided credentials were invalid.' };
+    return next(err);
+  }
+
+  // Creates the object to be sent
+  const safeUser = {
+    id: user.id,
+    email: user.email,
+    username: user.username,
+  };
+  // creates cookie
+  await setTokenCookie(res, safeUser);
+
+  return res.json({
+    user: safeUser
+  });
+}
+);
+
+module.exports = router;
