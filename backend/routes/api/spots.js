03b5d07bb60bbb01596485195fd078b2e1ad4d32
Andre <andreabad17@gmail.com>
2023-04-18 12:52:25 -0700

    Loaded the data succesfully for get all spots with aggregate data

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User, Review, ReviewImage, Spot, SpotImage, Booking, sequelize } = require('../../db/models');
@@ -44,30 +43,33 @@ const validatesNewSpot = [
 
 /*-------------------- Get All Spots --------------------*/
 router.get("/", async (req, res, next) => {
-  const allSpots = await Spot.findAll({ include: Review }) // Turns to an arr of Spots
+  const allSpots = await Spot.findAll({ include: [Review,SpotImage ]}) // Turns to an arr of Spots
   let Spots = [];
 
   allSpots.forEach(spot => {
-    let reviewArr = spot.Reviews
+    let currentSpot = spot.toJSON()
+    /*--- Getting the Average Star Rating ---*/
+    let reviewArr = currentSpot.Reviews
     let spotStars = []
     reviewArr.forEach(rev => {
       spotStars.push(rev.stars)
     })
-    // console.log(spotStars)
     let averageStars = spotStars.reduce((a, b) => a + b, 0) / 2
-    spot["averageStars"] = averageStars
-    spot["Reviews"] = null
-    Spots.push(spot)
+    currentSpot["averageStars"] = averageStars
+
+    /*--- Checking if the there is a preview Image  */
+    if(currentSpot.SpotImages){ currentSpot["previewUrl"]= currentSpot.SpotImages[0].url}
+    else currentSpot["previewUrl"] = "No Images could be found"
+
+    delete currentSpot["Reviews"]
+    delete currentSpot["SpotImages"]
+    Spots.push(currentSpot)
   })
-
-
-
-
   res.json(Spots)
 })
 
 /*-------------------- Create New Spot --------------------*/
-router.post("/", requireAuth,validatesNewSpot, async (req, res, next) => {
+router.post("/", requireAuth, validatesNewSpot, async (req, res, next) => {
   const { address, city, state, country, lat, lng, name, description, price } = req.body
   const { user } = req;
   const newSpot = await Spot.create({
@@ -95,8 +97,8 @@ router.get("/current", requireAuth, async (req, res, next) => {
     }
   })
   res.json({ Spots: currentUserSpots })
-
 })
+
 /*---------------- Get Details of a Spot ----------------*/
 router.get("/:spotId", async (req, res, next) => {
   const specificSpot = await Spot.findByPk(req.params.spotId, {
@@ -104,17 +106,48 @@ router.get("/:spotId", async (req, res, next) => {
       { model: User, as: "Owner" },
       { model: SpotImage }
     ],
-    // attributes:
   })
 
   res.json(specificSpot)
 })
 
 /*-------------- Create New Image for a Spot --------------*/
-router.post("/:spotId/images", async(req,res,next)=>{
+router.post("/:spotId/images", requireAuth, async (req, res, next) => {
+  const specificSpot = await Spot.findByPk(req.params.spotId)
+  if (!specificSpot) {
+    // NEED TO FIGURE OUT HOW TO HANDLE ERROR FOR THIS ONE
+    res.status(404)
+    res.json({
+      message: "Could not spot Spot"
+    })
+    return next(err)
+  }
+  const { url, preview } = req.body
 
+  const newImage = await SpotImage.create({
+    spotId: parseInt(req.params.spotId),
+    url,
+    preview
+  })
+
+  res.json(newImage)
 })
 /*-------------------- Edit a Spot --------------------*/
+router.put("/:spotId", requireAuth, async (req, res, next) => {
+  const { user } = req
+  const specificSpot = await Spot.findByPk(req.params.spotId)
+
+  //  Check if the current user's id is equal to the owner's id
+  if (user.id !== specificSpot.ownerId) {
+    console.log("error")
+    res.status(400)
+    res.json({ message: "You do not have access to edit this Spot's information" })
+    return next(err)
+  }
+
+
+  res.json({ working: "In Progress", specificSpot })
+})
 /*-------------------- Review a Spot --------------------*/
 
 module.exports = router;
