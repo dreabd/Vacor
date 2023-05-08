032102661319ac76297101ac047b069463c97ed3
Andre <andreabad17@gmail.com>
2023-05-08 11:08:25 -0700

    Finished the frontend validators and the form itself, workign on connecting it to the backend now

--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState,useEffect} from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import * as sessionActions from "../../store/session";
 import "./SignupForm.css";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 function SignupFormModal() {
   const dispatch = useDispatch();
@@ -12,8 +13,26 @@ function SignupFormModal() {
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [validationErrors,setValidationErrors] = useState({})
   const [errors, setErrors] = useState({});
   const { closeModal } = useModal();
+
+  const history = useHistory()
+
+
+  useEffect(()=>{
+    const errs = {}
+
+    if(!email.length) errs["email"] = "Email is required"
+    if(!username.length) errs["username"] = "Username is required"
+    if(!firstName.length) errs["firstName"] = "First name is required"
+    if(!lastName.length) errs["lastName"] = "Last name is required"
+    if(!password.length) errs["password"] = "Password is required"
+    if(!confirmPassword.length) errs["confirmPassword"] = "Confirm password is required"
+
+
+    setValidationErrors(errs)
+  },[email,username,firstName,password,confirmPassword])
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -29,6 +48,7 @@ function SignupFormModal() {
         })
       )
         .then(closeModal)
+        .then(history.push("/"))
         .catch(async (res) => {
           const data = await res.json();
           if (data && data.errors) {
@@ -48,6 +68,7 @@ function SignupFormModal() {
         <label>
           <span>Email {errors.email && <span className="errors">{errors.email}</span>}</span>
           <input
+            placeholder="Email"
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -57,6 +78,7 @@ function SignupFormModal() {
         <label>
           <span>Username {errors.username && <span className="errors">{errors.username}</span>}</span>
           <input
+            placeholder="Username"
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
@@ -69,6 +91,7 @@ function SignupFormModal() {
             {errors.firstName && <span className="errors">{errors.firstName}</span>}
           </span>
           <input
+            placeholder="First Name"
             type="text"
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
@@ -77,10 +100,11 @@ function SignupFormModal() {
         </label>
         <label>
           <span>
-          Last Name
-          {errors.lastName && <span className="errors">{errors.lastName}</span>}
+            Last Name
+            {errors.lastName && <span className="errors">{errors.lastName}</span>}
           </span>
           <input
+            placeholder="Last Name"
             type="text"
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
@@ -89,9 +113,10 @@ function SignupFormModal() {
         </label>
         <label>
           <span>
-          Password {errors.password && <span className="errors">{errors.password}</span>}
+            Password {errors.password && <span className="errors">{errors.password}</span>}
           </span>
           <input
+            placeholder="Password"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
@@ -100,15 +125,16 @@ function SignupFormModal() {
         </label>
         <label>
           <span> Confirm Password</span>
-           {errors.confirmPassword && (<span className="errors">{errors.confirmPassword}</span>)}
+          {errors.confirmPassword && (<span className="errors">{errors.confirmPassword}</span>)}
           <input
+            placeholder="Confirm Password"
             type="password"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
             required
           />
         </label>
-        <button type="submit">Sign Up</button>
+        <button disable={Object.values(validationErrors)}type="submit">Sign Up</button>
       </form>
     </>
   );
--- frontend/src/components/SpotsForm/index.js
+++ frontend/src/components/SpotsForm/index.js
@@ -8,7 +8,7 @@ import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
 const SpotForm = () => {
   const loggedIn = useSelector(state => state.session.user)
-  const ownerId = loggedIn.id
+  const ownerId = loggedIn?.id
   const dispatch = useDispatch()
 
   // ------- State Variables -------
@@ -16,61 +16,92 @@ const SpotForm = () => {
   const [address, setAddress] = useState("")
   const [city, setCity] = useState("")
   const [state, setState] = useState("")
-  const [descpt, setDescpt] = useState("")
-  const [name, setName] = useState("")
-  const [price, setPrice] = useState("")
-  const [photos, setPhotos] = useState([])
-  const [preview, setPreview] = useState("")
-  const [validationErrors, setValidationErrors] = useState({})
 
+  const [descpt, setDescpt] = useState("")
+
+  const [name, setName] = useState("")
+
+  const [price, setPrice] = useState("")
+
+  const [preview, setPreview] = useState("")
+
+  const [photo1, setPhoto1] = useState("")
+  const [photo2, setPhoto2] = useState("")
+  const [photo3, setPhoto3] = useState("")
+  const [photo4, setPhoto4] = useState("")
+
+  const [validationErrors, setValidationErrors] = useState({})
+  const [submit, setSubmitted] = useState(false)
+
+  // ------- Checking if its new Spot is valid -------
+  useEffect(() => {
+    const err = {}
+
+    if (!country.length) err["country"] = "Country is requred"
+    if (!address.length) err["address"] = "Address is requred"
+    if (!state.length) err["state"] = "State is requred"
+    if (!city.length) err["city"] = "City is requred"
+
+    if (descpt.length < 30) err["description"] = "Description needs a minimum of 30 characters"
+    if (!name.length) err["name"] = "Name is required"
+
+    if (!price.length) err["price"] = "Price is required"
+    if (parseInt(price) < 0) err["price"] = "Please provide a valid price"
+
+    const validURL = [".png", ".jpg", ".jpeg"]
+    if (!preview.length) err["preview"] = "Preview is required"
+    if (!validURL.includes(preview.substring(preview.length - 5))) err["previewImage"] = "Image URL must end in .png, .jpg, or .jpeg"
+    if (photo1.length && !validURL.includes(photo1.substring(photo1.length - 5))) err["Image"] = "Image URL must end in .png, .jpg, or .jpeg"
+    if (photo2.length && !validURL.includes(photo2.substring(photo2.length - 5))) err["Image"] = "Image URL must end in .png, .jpg, or .jpeg"
+    if (photo3.length && !validURL.includes(photo3.substring(photo3.length - 5))) err["Image"] = "Image URL must end in .png, .jpg, or .jpeg"
+    if (photo4.length && !validURL.includes(photo4.substring(photo4.length - 5))) err["Image"] = "Image URL must end in .png, .jpg, or .jpeg"
+
+    console.log(err)
+    setValidationErrors(err)
+
+  }, [country, address, city, state, descpt, name, price, preview, photo1, photo2, photo3, photo4])
+
+  // ---------Submit Functionality--------
   const onSubmit = e => {
     e.preventDefault();
-    // ------- Checking if its new Spot is valid -------
-    const err={}
+    setSubmitted(true)
 
-    if(!country.length) err["country"] = "Country is requred"
-    if(!address.length) err["address"] = "Address is requred"
-    if(!state.length) err["state"] = "State is requred"
-    if(!city.length) err["city"] = "City is requred"
+    if (Object.values(validationErrors).length) return validationErrors
 
-    if(descpt.length < 30) err["description"] = "Description needs a minimum of 30 characters"
-    if(!name.length) err["name"] = "Name is required"
+    // ------- For Posting the Spot Images -------
+    const spotImages = []
 
-    if(!price.length) err["price"] = "Price is required"
-    if(parseInt(price) < 0) err["price"] = "Please provide a valid price"
+    spotImages.push({ url: preview, preview: true })
 
-    const validURL = [".png",".jpg",".jpeg"]
-    if(preview.length) err["preview"] ="Preview is required"
-    if(!validURL.includes(preview.substring(preview.length-5))) err["Image"] = "Image URL must end in .png, .jpg, or .jpeg"
-    
-
-
+    if (photo1) spotImages.push({ url: photo1, preview: false })
+    if (photo2) spotImages.push({ url: photo2, preview: false })
+    if (photo3) spotImages.push({ url: photo3, preview: false })
+    if (photo4) spotImages.push({ url: photo4, preview: false })
 
     // ------- For Posting the new Spot -------
     const newSpot = {
-      country,
       address,
       city,
       state,
+      country,
       lattitude: Math.random() * 180,
       longitude: Math.random() * 180,
-      description: descpt,
       name,
+      description: descpt,
       price,
       ownerId
     }
 
-    // ------- For Posting the Spot Images -------
-    const SpotImages = []
-    photos.forEach(photo => {
-      const spotImg = {
-        url: photo,
-        preview: false,
-      }
-      SpotImages.push(spotImg)
-    })
+    // ------ Dispatching all the thunks that need be dispatched ------
+    // Thunk for creating a new spot
+    // ---- pass in the newspot for the thunk
+    // ---- need to return the id of the new spot
 
-    console.log({ SpotImages })
+    // Thunk for adding all the images
+    // --- Most likely will use a loop over the spotimages and dispatch all of those requests
+
+    // if there are errors then set validation errors to thsoe errors
+
 
 
     // -------Reseting the inputs-------
@@ -81,13 +112,22 @@ const SpotForm = () => {
     setDescpt("")
     setName("")
     setPrice("")
-    setPhotos([])
+
+    setPhoto1("")
+    setPhoto2("")
+    setPhoto3("")
+    setPhoto4("")
+
     setPreview("")
     setValidationErrors({})
+    setSubmitted(false)
+
+    // ------------IF THERE ARE NO ERRS ANYWHERE Redirect to New Spot-------------
+
   }
 
-  if (!loggedIn) return <Redirect to="/" />
 
+  if (!loggedIn) return <Redirect to="/" />
   return (
     <div className="create-new-spot-container">
 
@@ -98,32 +138,46 @@ const SpotForm = () => {
           <h4> Where's your place located </h4>
           <p> Guest will only get your exact address once they booked a reservation</p>
           <label className="new-spot-input-container" htmlFor="">
-            Country
+            <span>
+              Country {submit && validationErrors.country && <p className="errors">{validationErrors.country}</p>}
+            </span>
+
             <input
               type="text"
+              placeholder="Country"
               value={country}
               onChange={(e) => setCountry(e.target.value)} />
           </label>
           <label className="new-spot-input-container" htmlFor="">
-            Street Address
+
+            <span>
+              Street Address {submit && validationErrors.address && <p className="errors">{validationErrors.address}</p>}
+            </span>
             <input
               type="text"
+              placeholder="Street Address"
               value={address}
               onChange={(e) => setAddress(e.target.value)}
             />
           </label>
           <label className="new-spot-input-container" htmlFor="">
-            City
+            <span>
+              City {submit && validationErrors.city && <p className="errors">{validationErrors.city}</p>}
+            </span>
             <input
               type="text"
+              placeholder="City"
               value={city}
               onChange={(e) => setCity(e.target.value)}
             />
           </label>
           <label className="new-spot-input-container" htmlFor="">
-            State
+            <span>
+              State {submit && validationErrors.state && <p className="errors">{validationErrors.state}</p>}
+            </span>
             <input
               type="text"
+              placeholder="State"
               value={state}
               onChange={(e) => setState(e.target.value)}
             />
@@ -139,6 +193,7 @@ const SpotForm = () => {
             onChange={(e) => setName(e.target.value)}
             placeholder="Name of your spot"
           />
+          {submit && validationErrors.name && <p className="errors">{validationErrors.name}</p>}
         </div>
 
         <div className="set-description-container">
@@ -153,12 +208,16 @@ const SpotForm = () => {
             value={descpt}
             onChange={(e) => setDescpt(e.target.value)}
           ></textarea>
+          {submit && validationErrors.description && <p className="errors">{validationErrors.description}</p>}
+
         </div>
 
         <div className="set-price-container">
           <h4>Set a base price for your spot</h4>
           <p>Competitive pricing can help your listing stand out and rank higher
             in search results.</p>
+
+          {submit && validationErrors.price && <p className="errors">{validationErrors.price}</p>}
           <span>$ </span>
           <input
             placeholder="Price Per Night(USD)"
@@ -171,39 +230,48 @@ const SpotForm = () => {
         <div className="set-spot-images-container">
           <h4>Liven up your spot with photos</h4>
           <p>Submit a link to at least one photo to public to your spot</p>
-          <input
-            type="text"
-            placeholder="Preview Image URL"
-            value={preview}
-            onChange={e => setPreview(e.target.value)}
-          />
 
+          <label htmlFor="">
+            {submit && validationErrors.preview && <p className="errors">{validationErrors.preview}</p>}
+            {submit && validationErrors.previewImage && <p className="errors">{validationErrors.previewImage}</p>}
+            <input
+              type="text"
+              placeholder="Preview Image URL"
+              value={preview}
+              onChange={e => setPreview(e.target.value)}
+            />
+          </label>
+
+          {submit && validationErrors.Image && <p className="errors">{validationErrors.Image}</p>}
           <input
             type="text"
             placeholder="Image URL"
-            value={photos}
-            onChange={e => setPhotos([...photos, e.target.value])}
+            value={photo1}
+            onChange={e => setPhoto1(e.target.value)}
           />
 
+          {submit && validationErrors.Image && <p className="errors">{validationErrors.Image}</p>}
           <input
             type="text"
             placeholder="Image URL"
-            value={photos}
-            onChange={e => setPhotos([...photos, e.target.value])}
+            value={photo2}
+            onChange={e => setPhoto2(e.target.value)}
           />
 
+          {submit && validationErrors.Image && <p className="errors">{validationErrors.Image}</p>}
           <input
             type="text"
             placeholder="Image URL"
-            value={photos}
-            onChange={e => setPhotos([...photos, e.target.value])}
+            value={photo3}
+            onChange={e => setPhoto3(e.target.value)}
           />
 
+          {submit && validationErrors.Image && <p className="errors">{validationErrors.preview}</p>}
           <input
             type="text"
             placeholder="Image URL"
-            value={photos}
-            onChange={e => setPhotos([...photos, e.target.value])}
+            value={photo4}
+            onChange={e => setPhoto4(e.target.value)}
           />
         </div>
 
